30abfddebbe4543c38b46e598f20c14fdd832d56
HONG_NHAN\HONG NHAN <ritahitini@gmail.com>
2016-12-18 08:55:43 +0700

    Add Checkout va css

--- imports/page/checkout.js
+++ imports/page/checkout.js
@@ -18,7 +18,51 @@ class Checkout extends React.Component {
 
   render () {
     return (
-      <div></div>
+      
+<div className="container">
+  <div className="checkout">
+  <div className="col-xs-12">
+    <h3>Checkout</h3>
+  </div>
+  <div className="col-md-8">
+    <div className="col-sm-4">
+      <div className="row">
+        <div className="thumbnail">
+          <img src="http://www.w3schools.com/w3images/lights.jpg" alt="Cinque Terre" />
+        </div>
+      </div>
+    </div>
+    <div className="col-sm-8">
+      <div><b className="product-name">Iphone 7 Plus 128GB - New</b></div>
+      <div className="price-product">
+        Price: $200
+      </div>
+      <div className="number-products">
+        Quantum: 5
+      </div>
+    </div>
+  </div>
+  <div className="col-md-4 cost">
+  <div className="clearfix">
+    <span className="">Pay: <b>$200</b></span>
+  </div>
+  <div className="clearfix">
+    <span className="">Total having point: <b>100</b></span>
+  </div>
+  <div className="clearfix">
+    <span className="">Use point: <b className="usepoint">50</b></span>
+  </div>
+  <div className="divider"></div>
+  <div className="clearfix">
+    <span className="">Final: <b>$150</b></span>
+  </div>
+
+  </div>
+
+  <div className="clearfix"></div>
+</div>
+
+</div>
     )
   }
 }
